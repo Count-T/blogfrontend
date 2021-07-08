9ab977bcd780af53f52e5ae273f61cd090dd55b7
Tony <tony.lilui416@gmail.com>
2021-07-07 20:43:27 -0400

    added contacts email

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 function Contact() {
-  return <div>Contacts</div>;
+  return (
+    <div className="contact slowfade-in">
+      For any inquiries please email me at{" "}
+      <a className="email" href="mailto:info.countt@gmail.com">
+        📧info.countt@gmail.com📧
+      </a>
+    </div>
+  );
 }
 
 export default Contact;
